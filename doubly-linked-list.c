ad9cc79d6107772b612f7a4effe12455ce7fe456
gus-123 <65222175+gus-123@users.noreply.github.com>
2021-04-23 18:01:39 +0900

    Update doubly-linked-list.c

--- doubly-linked-list.c
+++ doubly-linked-list.c
@@ -177,7 +177,24 @@ void printList(headNode* h) {
  * list에 key에 대한 노드하나를 추가
  */
 int insertLast(headNode* h, int key) {
+	listNode* temp;  //연결리스트의 마지막 노드를 찾아서 참조하기 위해 노드를 할당
+	listNode* node = (listNode*)malloc(sizeof(listNode)); //삽입할 새로운 노드의 동적 메모리 할당
+	node->key = key; //새로운 노드의 데이터필드에 key 저장
+    
+	node->rlink = NULL;  //새로운 노드의 rlink는 null을 할당하여 연결을 해제
 
+	if(h->first == NULL) {  //head 노드가 null인 경우
+		h->first = node;  //head 노드가 새로운 노드를 참조
+
+		return 0;
+	}
+	
+	temp = h->first;  //temp 노드가 head를 참조
+	while(temp->rlink != NULL) {  //temp 노드의 rlink가 null이 아닌 경우
+		temp = temp->rlink;  //temp는 temp의 rlink를 참조하도록 함
+	}
+	temp->rlink = node;  //temp의 rlink가 새로운 노드를 참조하도록함
+	
 	return 0;
 }
 
@@ -187,8 +204,29 @@ int insertLast(headNode* h, int key) {
  * list의 마지막 노드 삭제
  */
 int deleteLast(headNode* h) {
-
-
+    listNode* preNode;
+    listNode* temp;
+        
+    if(h->first == NULL) {  //head 노드가 null인 경우 모든 노드가 삭제 되었으므로 리턴
+	
+		return 0;
+    }
+    
+	if(h->first->rlink == NULL) {  //head 노드의 rlink가 null인 경우
+		h->first = NULL;  //head에 null을 할당하여 남은 노드와의 연결을 해제
+    } 
+	else {  //head 노드의 rlink가 null이 아닌 경우
+		preNode = h->first; //preNode는 head가 가리키는 노드를 할당
+        temp = h->first->rlink;  //preNode의 다음 노드를 할당     
+            
+        while(temp->rlink != NULL) { //temp의 rlink가 null이 아닐 때 까지 한 노드씩 다음 노드로 이동
+			preNode = temp;  //preNode는 temp를 할당
+            temp = temp->rlink;  //temp는 temp의 다음 노드를 할당
+        }
+        preNode->rlink = NULL;  //preNode의 다음 노드인 temp로의 연결 해제
+    }
+	free(temp); //temp의 메모리 할당 해제
+	
 	return 0;
 }
 
