620aab36539f5812809af2301eccebb7c63a1a02
gus-123 <65222175+gus-123@users.noreply.github.com>
2021-04-23 18:37:05 +0900

    Update doubly-linked-list.c

--- doubly-linked-list.c
+++ doubly-linked-list.c
@@ -53,6 +53,7 @@ void printList(headNode* h);
 int main()
 {
 	char command;
+	printf("[----- [김현민]  [2018038088] -----]\n");
 	int key;
 	headNode* headnode=NULL;
 
@@ -121,7 +122,7 @@ int main()
 
 
 int initialize(headNode** h) {
-    //headNode가 공백이 아니면, freeNode를 호출하여 할당된 메모리 모두 해제
+	//headNode가 공백이 아니면, freeNode를 호출하여 할당된 메모리 모두 해제
 	if(*h != NULL) {
 		freeList(*h);
 	}
@@ -134,7 +135,7 @@ int initialize(headNode** h) {
 }
 
 int freeList(headNode* h){
-    listNode* p = h->first;  //p는 head가 가리키는 노드를 할당
+	listNode* p = h->first;  //p는 head가 가리키는 노드를 할당
 
 	listNode* prev = NULL;  //prev 노드를 초기화
 	while(p != NULL) {
@@ -204,27 +205,27 @@ int insertLast(headNode* h, int key) {
  * list의 마지막 노드 삭제
  */
 int deleteLast(headNode* h) {
-    listNode* preNode;
-    listNode* temp;
+	listNode* preNode;
+    	listNode* temp;
         
-    if(h->first == NULL) {  //head 노드가 null인 경우 모든 노드가 삭제 되었으므로 리턴
+    	if(h->first == NULL) {  //head 노드가 null인 경우 모든 노드가 삭제 되었으므로 리턴
 	
 		return 0;
-    }
+    	}
     
 	if(h->first->rlink == NULL) {  //head 노드의 rlink가 null인 경우
 		h->first = NULL;  //head에 null을 할당하여 남은 노드와의 연결을 해제
-    } 
+    	} 
 	else {  //head 노드의 rlink가 null이 아닌 경우
 		preNode = h->first; //preNode는 head가 가리키는 노드를 할당
-        temp = h->first->rlink;  //preNode의 다음 노드를 할당     
+        	temp = h->first->rlink;  //preNode의 다음 노드를 할당     
             
-        while(temp->rlink != NULL) { //temp의 rlink가 null이 아닐 때 까지 한 노드씩 다음 노드로 이동
+        	while(temp->rlink != NULL) { //temp의 rlink가 null이 아닐 때 까지 한 노드씩 다음 노드로 이동
 			preNode = temp;  //preNode는 temp를 할당
-            temp = temp->rlink;  //temp는 temp의 다음 노드를 할당
-        }
-        preNode->rlink = NULL;  //preNode의 다음 노드인 temp로의 연결 해제
-    }
+            		temp = temp->rlink;  //temp는 temp의 다음 노드를 할당
+        	}
+        	preNode->rlink = NULL;  //preNode의 다음 노드인 temp로의 연결 해제
+    	}
 	free(temp); //temp의 메모리 할당 해제
 	
 	return 0;
@@ -276,7 +277,18 @@ int deleteFirst(headNode* h) {
  * 리스트의 링크를 역순으로 재 배치
  */
 int invertList(headNode* h) {
+	listNode* nextNode = h->first; //head가 참조하는 첫번째 노드를 할당
+	listNode* currentNode = NULL;  //현재 위치를 참조하는 할당
+	listNode* preNode = NULL;  //이전 위치를 참조하는 할당
 
+	while (nextNode!= NULL) {  //head가 참조하는 첫번째 노드가 null이 아닌 경우
+		preNode = currentNode;  //preNode는 currentNode 위치로 이동
+		currentNode = nextNode;  //currentNode 위치로 이동
+		nextNode = nextNode->rlink;  //nextNode는 다음 노드 위치로 이동
+		currentNode->rlink = preNode;  //currentNode의 rlink에 preNode를 할당하여 역순 만듦
+	}
+	h->first = currentNode;  //currentNode가 마지막 노드를 가리킬 때, head는 currentNode를 참조 함
+	
 	return 0;
 }
 
@@ -284,7 +296,32 @@ int invertList(headNode* h) {
 
 /* 리스트를 검색하여, 입력받은 key보다 큰값이 나오는 노드 바로 앞에 삽입 */
 int insertNode(headNode* h, int key) {
+	listNode* preNode = h->first; //head가 가리키는 노드를 할당
+    	listNode* node = (listNode*)malloc(sizeof(listNode)); //삽입할 새로운 노드의 동적 메모리 할당
+	node->key = key; //새로운 노드의 데이터필드에 key 저장
 
+    	node->rlink = NULL;  //새로운 노드의 rlink는 null을 할당하여 연결을 해제
+
+    	if (h->first == NULL) { //head 노드가 null인 경우
+        	h->first = node;  //head 노드가 새로운 노드를 참조
+    	}
+    	else { //head 노드가 null이 아닌 경우
+		if (node->key < h->first->key) { //삽입할 위치가 맨 앞인 경우
+			node->rlink = h->first->rlink;  //head는 head의 다음 노드를 참조하도록 함
+            		h->first->rlink = node;  //head의 rlink가 다음 노드를 참조하도록 함
+        	}
+        	else {
+            		while (preNode->rlink) { //preNode 노드의 rlink인 경우
+				if (node->key < preNode->rlink->key) {  //삽입할 위치 찾기
+					break;
+				}
+                		preNode = preNode->rlink; //preNode는 preNode의 다음 노드를 할당
+            		}
+            		node->rlink = preNode->rlink;  //새로운 노드의 rlink가 preNode의 다음 노드를 참조하도록 함
+            		preNode->rlink = node; //preNode의 rlink가 새로운 노드를 참조하도록 함
+        	}
+    	}
+	
 	return 0;
 }
 
@@ -293,6 +330,24 @@ int insertNode(headNode* h, int key) {
  * list에서 key에 대한 노드 삭제
  */
 int deleteNode(headNode* h, int key) {
+	listNode* preNode = h->first; //head가 가리키는 노드를 할당
+	listNode* temp = h->first->rlink; //head가 가리키는 노드의 다음 노드를 할당
 
+	if (key == preNode->key) { //첫번째 노드의 데이터와 일치하는 경우
+		h->first = preNode->rlink; //head가 preNode의 다음 노드를 참조하도록 함
+		preNode->rlink = NULL; //preNode의 rlink는 null을 할당하여 연결을 해제
+	}
+	else {
+		while (temp != NULL) { //temp가 null일 때까지 반복하여 탐색
+            		if (key == preNode->rlink->key) {  //삭제할 위치 찾기
+                		preNode->rlink = temp->rlink;  //preNode의 rlink는 temp의 다음 노드를 참조
+                
+                		return 0;
+            		}
+            		preNode = preNode->rlink;  //preNode에 preNode에 다음 노드를 할당
+        	}
+	}
+	free(temp); //temp의 메모리 할당 해제
+	
 	return 1;
 }
